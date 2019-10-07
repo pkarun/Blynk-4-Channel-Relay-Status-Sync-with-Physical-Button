92a6091a31fcd0859a002afb32c6ba127640ea61
Arun desktop <pkarun29@gmail.com>
2019-10-07 20:19:41 +0530

    changed file structure

--- Blynk-4-Channel-Relay-Status-Sync-with-Physical-Button/settings.h
+++ Blynk-4-Channel-Relay-Status-Sync-with-Physical-Button/settings.h
@@ -17,12 +17,12 @@
 #define RELAY_PIN_3       4   //D2
 #define RELAY_PIN_4       5   //D1 
 
-/* NodeMCU to Push Button switch  */ 
+/* NodeMCU to Push Button switch  */
 /*
   For push button/switch action I don't have push button so I will be keep those wire open and 
   touch each of these wires with GND wire on NodeMCU to perform On and OFF action.
  */
-
+ 
 #define PUSH_BUTTON_1     2   //D4
 #define PUSH_BUTTON_2    14   //D5
 #define PUSH_BUTTON_3    13   //D7
@@ -39,24 +39,8 @@
 #define VPIN_BUTTON_4    V15         
 
 /***************************************************
- *       Blynk Settings 
- **************************************************/
- 
-#define AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" //Live
-
-
-/***************************************************
- *        WiFi Settings
- **************************************************/
-  
-#define WIFI_SSID "xxxxxxxxxxxx"
-#define WIFI_PASS "xxxxxxxxxxxx"
-
-
-/***************************************************
  *        Server Settings
  **************************************************/
       
-#define OTA_HOSTNAME "4 Channel Relay with Push Button Sync"
-#define SERVER "xxx.xxx.xx.xxx"
-#define PORT 8080
+#define OTA_HOSTNAME "4 Channel Relay with Button Sync"
+ 
